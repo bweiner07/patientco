4d98984689f0b723243122890b8aecbd32943bdd
bweiner07 <bradley.weiner1@gmail.com>
2021-07-02 20:48:06 -0500

    'init''

---
+++ src/main/java/com/patientco/bradhomework/BradHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.patientco.bradhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patientco/bradhomework/BradHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patientco.bradhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
